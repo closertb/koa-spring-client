7be9bb3c756e5a5f8458fb0d6e8a797ee15a98c8
closertb <closertb@sina.com>
2020-08-29 10:46:38 +0800

    feat: do a test

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,10 @@
 const AntdDayjsWebpackPlugin = require('antd-dayjs-webpack-plugin');
 
 module.exports = (config) => {
+  // config.entry = './md5.js';
+  // config.output.library =  'md5';
+  // config.output.libraryTarget = 'umd';
+  // config.output.filename = 'md5.js';
   // 配置按需加载，单独打包，加速加载时间
   config.optimization.splitChunks.chunks = 'initial';
   config.plugins.push(new AntdDayjsWebpackPlugin());
