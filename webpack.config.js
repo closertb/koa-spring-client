ed8c0e787f41ec2cc2b8c6008d9d5e1af7215dd2
closertb <closertb@sina.com>
2020-11-20 21:44:33 +0800

    feat: add some config

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,16 @@ module.exports = (config) => {
   // config.output.filename = 'md5.js';
   // 配置按需加载，单独打包，加速加载时间
   config.optimization.splitChunks.chunks = 'initial';
+  // Object.assign(config.devServer, {
+  //   proxy: {
+  //     '/api': {
+  //       target: 'http://deploy.closertb.site',
+  //       changeOrigin: true,
+  //       secure: false,
+  //       pathRewrite: {'^/api' : ''}
+  //     }
+  //   }
+  // });
   config.plugins.push(new AntdDayjsWebpackPlugin());
   return config;
 }
